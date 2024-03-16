b6f17fb6e4f2dbe33c2b147b4ec3fbb872462e2a
Islombek <whoisit864@gmail.com>
2024-03-16 15:25:17 +0300

    create project

---
+++ EffectiveMobileTest/EffectiveMobileTest/EffectiveMobileTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  EffectiveMobileTestApp.swift
+//  EffectiveMobileTest
+//
+//  Created by Islombek Gofurov on 16/03/24.
+//
+
+import SwiftUI
+
+@main
+struct EffectiveMobileTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
